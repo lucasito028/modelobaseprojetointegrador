340dd359286b3adb6addebae472549935d299598
lucasito028 <lucastoshihiroito28@gmail.com>
2024-04-07 22:04:17 -0300

    Estruturando Composer

---
+++ App/Controller/TestController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Controller;
+
+class TestController {
+    public function index() {
+        echo "Test Controller is working!";
+    }
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+    use Core\Routes;
+
+    require_once __DIR__ . '/vendor/autoload.php';
+
+    $rout = new Routes();
+
